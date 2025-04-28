118de776b4ed64633a924df291b4d41ad428a361
2b21a0b858193313 <8aa4b9a009df7dd1>
2025-04-28 10:21:24 +0000

    fix bug

--- pack.sh
+++ pack.sh
@@ -1,3 +1,3 @@
 find . | grep -E "(/__pycache__$|\.pyc$|\.pyo$)" | xargs rm -rf
 rm -rf mcp-agent.zip
-zip -r mcp-agent.zip src Dockerfile requirements.txt
+zip -r mcp-agent.zip utils main.py Dockerfile requirements.txt
